d7eb9f2234045561a2925039835264d62eaab5d0
Ye-Yint-Nyo-Hmine <yeyintnyohmine143@gmail.com>
2023-11-12 11:18:41 -0500

    Finished structure for pygame

--- main.py
+++ main.py
@@ -6,9 +6,26 @@ import sys
 
 pygame.init()
 
+SCREEN_SIZE_FACTOR = 80
+WIDHT, HEIGHT = 16*SCREEN_SIZE_FACTOR, 9*SCREEN_SIZE_FACTOR
+WIN = pygame.display.set_mode((WIDHT, HEIGHT))
+
+
 
 def main():
-    pass
+
+    run = True
+    FPS = 60
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+    pygame.quit()
 
 
 
