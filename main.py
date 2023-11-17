cdf923cd6e283b85138dedfc9b0c4c980b52fe23
Ye-Yint-Nyo-Hmine <yeyintnyohmine143@gmail.com>
2023-11-16 20:05:46 -0500

    Removed particle collision, added 3 platforms to create a box. Tidy up and cleaned the code while commenting everything out

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 # Fluid Dynamic Simulation
 import pygame
 import sys
+import random
 from particles import Particle
 from non_gravity_objects import Platform
-import random
-
 
 # initialize pygame
 pygame.init()
@@ -15,29 +14,35 @@ WIDTH, HEIGHT = 16*SCREEN_SIZE_FACTOR, 9*SCREEN_SIZE_FACTOR
 WIN_CENTER = (WIDTH/2, HEIGHT/2)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
-
-def generate_group_particles(surface, num_x, num_y):
-    for rows in range(num_y):
-        for particles_x in range(num_x):
-            particles.add(Particle((100 + particles_x*30, 0+rows*40), 10, 2, [0, random.randint(-3, 3)]))
-
-
 # initialize class dependencies
-particles = pygame.sprite.Group()
-particles.add(Particle((WIN_CENTER[0], 50), 10, 2, [0, 0]))
-particles.add(Particle((WIN_CENTER[0]-200, 50), 10, 2, [5, 0]))
 
+# Created Sprite Group to contain platforms in
 non_gravity_objects = pygame.sprite.Group()
-non_gravity_objects.add(Platform([100, WIN_CENTER[1]+100], [WIDTH-200, 10], 10000)) #increasing mass to 1000 makes platform static
+
+# Created Sprite Group to contain particles in
+particles = pygame.sprite.Group()
+
+# create standard properties for particles
+PARTICLE_MASS = 2
+PARTICLE_RADIUS = 10
 
 
-#! I am describing the changes in the github commit.
+# Create objects on surface with a boundary of width and height
+non_gravity_objects.add(Platform([0, HEIGHT-10], [WIDTH, 10], invincible=True))
+non_gravity_objects.add(Platform([0, 0], [WIDTH, 10], invincible=True))
+non_gravity_objects.add(Platform([0, 0], [10, HEIGHT], invincible=True))
+non_gravity_objects.add(Platform([WIDTH-10, 0], [10, HEIGHT], invincible=True))
+
+particles.add(Particle((WIN_CENTER[0], 50), PARTICLE_RADIUS, PARTICLE_MASS, [0, 0]))
+particles.add(Particle((WIN_CENTER[0]-200, 50), PARTICLE_RADIUS, PARTICLE_MASS, [5, 0]))
+
 
 
 def gameUpdate(surface):
     """
     :return: Update the game, and display objects onto screen
     """
+    # recolor the surface every frame
     surface.fill("black")
 
     #? This is an attempt to solve the issue with ball falling through the platform (the same problem you had in the first prototype)
@@ -51,99 +56,110 @@ def gameUpdate(surface):
     for ngo in non_gravity_objects:
         positions[ngo] = [ngo.rect.x, ngo.rect.y]
 
-
+    # Update all the sprites
     particles.update()
     non_gravity_objects.update()
-    
-    #* Checking for collisions
-    #? Maybe move to another script
-    #> I agree, i also think it should be move to another script
-        #* Collision detection between particles
-    #TODO: The particles stack on each other, needs to be fixed
-    collision_damping_constant = 0.7 #* lost of kinetic energy constant
-    for p1 in particles:
-        for p2 in particles:
-            if p1 != p2:
-                if pygame.sprite.collide_circle(p1, p2):
-                    v1, v2 = p1.velocity[0], p2.velocity[0]
-                    m1, m2 = p1.mass, p2.mass
 
-                    p1.velocity[0] = ((v1*(m1-m2) + 2*m2*v2) / (m1 + m2)) * collision_damping_constant
-                    p2.velocity[0] = ((v2*(m2-m1) + 2*m1*v1) / (m1+m2)) * collision_damping_constant
+    # constant variable for Collision Damping
+    COLLISION_DAMPING_CONSTANT = 0.5
 
-
-                    v1, v2 = p1.velocity[1], p2.velocity[1]
-                    m1, m2 = p1.mass, p2.mass
-
-                    p1.velocity[1] = ((v1*(m1-m2) + 2*m2*v2) / (m1 + m2)) * collision_damping_constant
-                    p2.velocity[1] = ((v2*(m2-m1) + 2*m1*v1) / (m1+m2)) * collision_damping_constant
-
-                    p1.rect.x, p1.rect.y = positions[p1]
-                    p2.rect.x, p2.rect.y = positions[p2]
-    
-
-        #* Collision detection between particles and non_gravity_objects
+    #* Collision detection between particles and non_gravity_objects
     collisions = pygame.sprite.groupcollide(particles, non_gravity_objects, False, False)
     for particle in collisions.keys():
         v1, v2 = particle.velocity[1], collisions[particle][0].velocity[1]
         m1, m2 = particle.mass, collisions[particle][0].mass
 
-        particle.velocity[1] = ((v1*(m1 - m2) + 2*m2*v2) / (m1 + m2)) * collision_damping_constant #multiplying by 0.9 simulates the loss of kinetic energy in real world
-        collisions[particle][0].velocity[1] = ((v2*(m2-m1) + 2*m1*v1) / (m1+m2)) * collision_damping_constant
+        particle.velocity[1] = ((v1*(m1 - m2) + 2*m2*v2) / (m1 + m2)) * COLLISION_DAMPING_CONSTANT
+        collisions[particle][0].velocity[1] = ((v2*(m2-m1) + 2*m1*v1) / (m1+m2)) * COLLISION_DAMPING_CONSTANT
 
         #* This resets position of objects after collision (if you want to know how that works text me)
         particle.rect.x, particle.rect.y = positions[particle]
         collisions[particle][0].rect.x, collisions[particle][0].rect.y = positions[collisions[particle][0]]
-    
-
-
 
     #* Finally drawing objects onto screen
     particles.draw(WIN)
     non_gravity_objects.draw(WIN)
 
+    # Update pygame display
     pygame.display.update()
 
+def getKeyPresses(surface):
+    # Get inputs from keyboard and neat up the code
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_r]:  # reset and clear the screen when r is pressed
+        reset(surface)
+
+    if keys[pygame.K_n]:
+        print(len(particles))
+
+    if keys[pygame.K_g]:
+        generate_group_particles(surface)
+
+    if not keys[pygame.K_s]:  # * freezes frame when key s is being pressed
+        gameUpdate(surface)
 
 def reset(surface):
     """
     :return: reset the program by clearing the screen
     """
+    # removing particles from the screen
     for particle in particles:
         particles.remove(particle)
+
+
+def generate_group_particles(surface, num_x=37, num_y=10, pos=(100, 50)):
+    """
+    :param surface: Game surface to create particles on
+    :param num_x: the number of particles on each row
+    :param num_y: the number of rows
+    :return:
+    """
+    # for each particle in set num x and num y, create particles
+    for rows in range(num_y):
+        for column in range(num_x):
+            particles.add(Particle((pos[0] + column*30, pos[1]+rows*40), 10, 2, [random.randrange(-10, 10), random.randrange(-10, 10)]))
+
+
 
 def main():
     """
     :return: run the main program
     """
+    # game run variable is set
     run = True
+
+    # Set standard FPS to 60 for running the program
     FPS = 60
     clock = pygame.time.Clock()
 
+    generate_group_particles(WIN)
+
+    # main game loop
     while run:
-        clock.tick(FPS)
         for event in pygame.event.get():
+            # quit pygame
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            # get mouse inputs
             if event.type == pygame.MOUSEBUTTONDOWN:
-                mousepos = pygame.mouse.get_pos()
-                particles.add(Particle(mousepos, 10, 2, [random.randint(-3, 3), random.randint(-3, 3)]))
-
-        # Get inputs from keyboard
-        keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_r]: # reset and clear the screen when r is pressed
-            reset(WIN)
+                mouse_pos = pygame.mouse.get_pos()
+                particles.add(Particle(mouse_pos, PARTICLE_RADIUS, PARTICLE_MASS, [random.randrange(-10, 10), random.randrange(-10, 10)]))
 
 
-        if not keys[pygame.K_s]: #* freezes frame when key s is being pressed
-            gameUpdate(WIN)
+        # function for all the key presses to clean up the code
+        getKeyPresses(WIN)
 
+        # tick at every FPS
+        clock.tick(FPS)
+
+    # quit pygame when the program completes
     pygame.quit()
 
 
 
 
 if __name__ == "__main__":
+    # run the main function
     main()
