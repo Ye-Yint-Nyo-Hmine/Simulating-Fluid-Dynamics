c3b212d04f2e732511920f23c7cc271bb5c7910d
Filip D <filidvor18@gmail.com>
2023-11-18 14:05:49 +0100

    Moved updates of objects to another script

---
+++ objects/other_objects.py
@@ -0,0 +1,40 @@
+import pygame
+
+#* On these objects, gravity is not applied
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, position, size, invincible=False, mass=1000, velocity=[0, 0], color="white"):
+        super().__init__()
+
+        self.size = size
+        self.mass = mass
+        self._velocity = velocity
+        self.color = color
+
+        self.image = pygame.Surface(size)
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = position
+
+        self.gravity = 0 # added for a feature for future use case
+
+        self.invincible = invincible # added because current platforms do not get affected by gravity nor momentum
+
+    def update(self):
+        self.velocity[1] += self.gravity
+        # if you dont mind, I deleted this, since the condition in setter prevents the velocity from changing
+        self.rect.x += self._velocity[0]
+        self.rect.y += self._velocity[1]
+
+
+
+    @property
+    def velocity(self):
+        return self._velocity
+
+    @velocity.setter
+    def velocity(self, new_velocity):
+        if not self.invincible:
+            self._velocity = new_velocity
+
+
---
+++ objects/updates.py
@@ -0,0 +1,37 @@
+import pygame
+
+
+def updates(particles, non_gravity_objects):
+    positions = {}
+    for particle in particles:
+        positions[particle] = [particle.rect.x, particle.rect.y]
+    for ngo in non_gravity_objects:
+        positions[ngo] = [ngo.rect.x, ngo.rect.y]
+
+    # Update all the sprites
+    particles.update()
+    non_gravity_objects.update()
+
+    # constant variable for Collision Damping
+    COLLISION_DAMPING_CONSTANT = 0.5
+
+    #* Collision detection between particles and non_gravity_objects
+    collisions = pygame.sprite.groupcollide(particles, non_gravity_objects, False, False)
+    for particle in collisions.keys():
+        v1, v2 = particle.velocity[1], collisions[particle][0].velocity[1]
+        m1, m2 = particle.mass, collisions[particle][0].mass
+
+        particle.velocity[1] = ((v1*(m1 - m2) + 2*m2*v2) / (m1 + m2)) * COLLISION_DAMPING_CONSTANT
+        collisions[particle][0].velocity = [collisions[particle][0].velocity[0], ((v2*(m2-m1) + 2*m1*v1) / (m1+m2)) * COLLISION_DAMPING_CONSTANT]
+
+        v1, v2 = particle.velocity[0], collisions[particle][0].velocity[0]
+        particle.velocity[0] = ((v1*(m1 - m2) + 2*m2*v2) / (m1 + m2)) * COLLISION_DAMPING_CONSTANT
+        collisions[particle][0].velocity = [((v2*(m2-m1) + 2*m1*v1) / (m1+m2)) * COLLISION_DAMPING_CONSTANT, collisions[particle][0].velocity[1]]
+
+
+
+        #* This resets position of objects after collision (if you want to know how that works text me)
+        particle.rect.x, particle.rect.y = positions[particle]
+        collisions[particle][0].rect.x, collisions[particle][0].rect.y = positions[collisions[particle][0]]
+    
+    return particles, non_gravity_objects
