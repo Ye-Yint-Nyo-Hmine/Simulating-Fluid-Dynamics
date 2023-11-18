9ead825fe1de6353b34300b2a317308d2d8ab0e5
Filip D <filidvor18@gmail.com>
2023-11-18 14:26:57 +0100

    organizing code

--- main.py
+++ main.py
@@ -36,14 +36,17 @@ other_objects.add(Platform([WIDTH-10, 0], [10, HEIGHT], invincible=True))
 
 
 def gameUpdate(surface):
-    global particles
-    global other_objects
     """
     :return: Update the game, and display objects onto screen
     """
-    # recolor the surface every frame
+
+    global particles
+    global other_objects
+
+    # *Recolor the surface every frame
     surface.fill("black")
 
+    #* Updates the objects
     particles, other_objects, = updates(particles, other_objects)
 
     #* Finally drawing objects onto screen
@@ -53,29 +56,16 @@ def gameUpdate(surface):
     # Update pygame display
     pygame.display.update()
 
-def getKeyPresses(surface):
-    # Get inputs from keyboard and neat up the code
-    keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_r]:  # reset and clear the screen when r is pressed
-        reset(surface)
-
-    if keys[pygame.K_n]:
-        print(len(particles))
-
-    if keys[pygame.K_g]:
-        generate_group_particles(surface)
-
-    if not keys[pygame.K_s]:  # * freezes frame when key s is being pressed
-        gameUpdate(surface)
 
 def reset(surface):
     """
     :return: reset the program by clearing the screen
     """
-    # removing particles from the screen
+    #* removing particles from the screen
     for particle in particles:
         particles.remove(particle)
+
 
 
 def generate_group_particles(surface, num_x=37, num_y=10, pos=(100, 50)):
@@ -85,10 +75,29 @@ def generate_group_particles(surface, num_x=37, num_y=10, pos=(100, 50)):
     :param num_y: the number of rows
     :return:
     """
-    # for each particle in set num x and num y, create particles
+    #* for each particle in set num x and num y, create particles
     for rows in range(num_y):
         for column in range(num_x):
             particles.add(Particle((pos[0] + column*30, pos[1]+rows*40), 10, 2, [random.randrange(-10, 10), random.randrange(-10, 10)]))
+
+
+
+
+def getKeyPresses(surface):
+    #* Get inputs from keyboard and run some functions
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_r]:  #* reset and clear the screen when r is pressed
+        reset(surface)
+
+    if keys[pygame.K_n]:
+        print(len(particles)) #* displays number of particles on screen
+
+    if keys[pygame.K_g]:
+        generate_group_particles(surface) #* generates particles
+
+    if not keys[pygame.K_s]:  # * freezes frame when key s is being pressed
+        gameUpdate(surface)
 
 
 
@@ -96,31 +105,31 @@ def main():
     """
     :return: run the main program
     """
-    # game run variable is set
+    # *game run variable is set
     run = True
 
-    # Set standard FPS to 60 for running the program
+    # *Set standard FPS to 60 for running the program
     FPS = 60
     clock = pygame.time.Clock()
     highest_fps = 0
 
     generate_group_particles(WIN)
 
-    # main game loop
+    #* Main game loop
     while run:
         for event in pygame.event.get():
-            # quit pygame
+            # *quit pygame
             if event.type == pygame.QUIT:
                 print(highest_fps)
                 pygame.quit()
                 sys.exit()
-            # get mouse inputs
+            # *get mouse inputs
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pos = pygame.mouse.get_pos()
                 particles.add(Particle(mouse_pos, PARTICLE_RADIUS, PARTICLE_MASS, [random.randrange(-10, 10), random.randrange(-10, 10)]))
 
 
-        # function for all the key presses to clean up the code
+        #* function for all the key presses (also updates the screen)
         getKeyPresses(WIN)
 
         fps = clock.get_fps()
