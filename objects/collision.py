fa2ebbaa7286abf585df9c95ed1f2295590caaea
Ye-Yint-Nyo-Hmine <yeyintnyohmine143@gmail.com>
2023-11-23 17:34:13 -0500

    created a more mathematical approach to collision damping factor

--- objects/collision.py
+++ objects/collision.py
@@ -11,8 +11,8 @@ def collision(particles, non_gravity_objects):
     particles.update()
     non_gravity_objects.update()
 
-    # constant variable for Collision Damping
-    COLLISION_DAMPING_CONSTANT = 0.65
+    #* a more mathematical variable for Collision Damping
+    COLLISION_DAMPING_CONSTANT = lambda mass: (mass/2)
 
     #* Collision detection between particles and non_gravity_objects
     collisions = pygame.sprite.groupcollide(particles, non_gravity_objects, False, False)
@@ -21,19 +21,19 @@ def collision(particles, non_gravity_objects):
         m1, m2 = particle.mass, collisions[particle][0].mass
 
         if particle.rect.y - particle.radius > collisions[particle][0].rect.y:
-            particle.velocity[1] = -((v1*(m1 - m2) + 2*m2*v2) / (m1 + m2)) * COLLISION_DAMPING_CONSTANT
-            collisions[particle][0].velocity = [collisions[particle][0].velocity[0], ((v2*(m2-m1) + 2*m1*v1) / (m1+m2)) * COLLISION_DAMPING_CONSTANT]
+            particle.velocity[1] = -((v1*(m1 - m2) + 2*m2*v2) / (m1 + m2)) * COLLISION_DAMPING_CONSTANT(particle.mass)
+            collisions[particle][0].velocity = [collisions[particle][0].velocity[0], ((v2*(m2-m1) + 2*m1*v1) / (m1+m2)) * COLLISION_DAMPING_CONSTANT(particle.mass)]
         else:
-            particle.velocity[1] = ((v1 * (m1 - m2) + 2 * m2 * v2) / (m1 + m2)) * COLLISION_DAMPING_CONSTANT
-            collisions[particle][0].velocity = [collisions[particle][0].velocity[0], ((v2 * (m2 - m1) + 2 * m1 * v1) / (m1 + m2)) * COLLISION_DAMPING_CONSTANT]
+            particle.velocity[1] = ((v1 * (m1 - m2) + 2 * m2 * v2) / (m1 + m2)) * COLLISION_DAMPING_CONSTANT(particle.mass)
+            collisions[particle][0].velocity = [collisions[particle][0].velocity[0], ((v2 * (m2 - m1) + 2 * m1 * v1) / (m1 + m2))]
         if particle.rect.x - particle.radius > collisions[particle][0].rect.x:
             v1, v2 = particle.velocity[0], collisions[particle][0].velocity[0]
-            particle.velocity[0] = -((v1*(m1 - m2) + 2*m2*v2) / (m1 + m2)) * COLLISION_DAMPING_CONSTANT
-            collisions[particle][0].velocity = [((v2*(m2-m1) + 2*m1*v1) / (m1+m2)) * COLLISION_DAMPING_CONSTANT, collisions[particle][0].velocity[1]]
+            particle.velocity[0] = -((v1*(m1 - m2) + 2*m2*v2) / (m1 + m2)) * COLLISION_DAMPING_CONSTANT(particle.mass)
+            collisions[particle][0].velocity = [((v2*(m2-m1) + 2*m1*v1) / (m1+m2)), collisions[particle][0].velocity[1]]
         else:
             v1, v2 = particle.velocity[0], collisions[particle][0].velocity[0]
-            particle.velocity[0] = ((v1 * (m1 - m2) + 2 * m2 * v2) / (m1 + m2)) * COLLISION_DAMPING_CONSTANT
-            collisions[particle][0].velocity = [((v2 * (m2 - m1) + 2 * m1 * v1) / (m1 + m2)) * COLLISION_DAMPING_CONSTANT,collisions[particle][0].velocity[1]]
+            particle.velocity[0] = ((v1 * (m1 - m2) + 2 * m2 * v2) / (m1 + m2)) * COLLISION_DAMPING_CONSTANT(particle.mass)
+            collisions[particle][0].velocity = [((v2 * (m2 - m1) + 2 * m1 * v1) / (m1 + m2)),collisions[particle][0].velocity[1]]
 
         #* This resets position of objects after collision (if you want to know how that works text me)
         particle.rect.x, particle.rect.y = positions[particle]
