93d6557032550929f120666139bf2752b6835107
Ye-Yint-Nyo-Hmine <yeyintnyohmine143@gmail.com>
2023-11-24 21:05:07 -0500

    Readjust the calculations due to an error

--- densityCalc.py
+++ densityCalc.py
@@ -6,17 +6,16 @@ import pygame
 def smoothingCurve(radius, distance):
     volume = (math.pi * (radius**8))/4
     value = max(0, (radius**2) - (distance**2))
-    return value**3
+    return (value**3)/volume
 
 def calculateDensity(WIN, particles, sample_point, smoothing_radius=5):
     density = 0
     pygame.draw.circle(WIN, (0, 255, 255),
-                       (sample_point), smoothing_radius, width=5)
+                       (sample_point), smoothing_radius, width=smoothing_radius-8)
 
     for particle in particles:
         distance = math.sqrt((particle.center[0]-sample_point[0])**2 + (particle.center[1]-sample_point[1])**2)
-        #* 10**8.86271  is a constant I found out after many trials and error which make the center of the particle max density exactly 1.0000
-        influence = smoothingCurve(smoothing_radius, distance)/10**8.86271
+        influence = smoothingCurve(smoothing_radius, distance)*1000
         density += (particle.mass * influence)
 
     return round(density, 4)
